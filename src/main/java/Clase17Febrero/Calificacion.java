1464aa95dd217d34776977c3470971e344d06632
RobertoCuevasP <48989846+RobertoCuevasP@users.noreply.github.com>
2021-02-19 07:37:47 -0400

    Ejercicio 2

---
+++ src/main/java/Clase17Febrero/Calificacion.java
@@ -0,0 +1,13 @@
+package Clase17Febrero;
+
+public class Calificacion {
+
+    public int calculoNota(int n1, int n2, int n3){
+        int ans = 0;
+        if(n1 > 0 && n2 > 0 && n3 > 0){
+
+        }
+
+        return ans;
+    }
+}
---
+++ src/test/java/calcularHoraFechaTest/CalcularHoraFechaParameters.java
@@ -0,0 +1,13 @@
+package calcularHoraFechaTest;
+
+import ejercicio2.CalcularHoraFecha;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalcularHoraFechaParameters {
+}
